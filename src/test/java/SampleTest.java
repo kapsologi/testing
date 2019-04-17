dcdde70e6eb8ff3d691b39c3400b0455678600a1
kapsologi <kapil.c.kumar@capgemini.com>
2019-04-17 16:48:42 +0800

    testing

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,5 +1,6 @@
 public class SampleTest {
     public static void main(String[] args) {
         System.out.println("This is to test java test");
+        System.out.println("testing another");
     }
 }
